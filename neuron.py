5ae933371954d99518b628a3495038f26d134865
Evgeny Kolyakov <thenetfreaker@gmail.com>
2026-09-14 14:58:04 +0300

    Fixed import 0.1.1

--- neuron.py
+++ neuron.py
@@ -8,7 +8,9 @@ class Neuron:
     def __init__(self, body_id, name):
         self.body_id = body_id
         self.name = name
+        # A potential value representing the neuron's readiness to fire.
         self.potential = 0.0
+        # A value representing the neuron's recent activity.
         self.activity = 0.0
         # Region-of-interest label (if known). Kept as a small string
         # to allow region-aware routing/weighting in the Brain.
